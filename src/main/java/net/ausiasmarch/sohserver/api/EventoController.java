09bf5b575b8f80b8b8d7e7621ff9b70f26c6328d
mario <newmariotomas@gmail.com>
2022-12-16 17:10:15 +0100

    cambios fallando

--- src/main/java/net/ausiasmarch/sohserver/api/EventoController.java
+++ src/main/java/net/ausiasmarch/sohserver/api/EventoController.java
@@ -1,6 +1,25 @@
 package net.ausiasmarch.sohserver.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import net.ausiasmarch.sohserver.entity.EventoEntity;
+import net.ausiasmarch.sohserver.service.EventoService;
+
+@RestController
+@RequestMapping("/evento")
 public class EventoController {
-    
+    @Autowired
+    EventoService oEventoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventoEntity> get(@PathVariable(value = "id") Long id) {
+        return new ResponseEntity<>(oEventoService.get(id), HttpStatus.OK);
+    }
+
 }
--- src/main/java/net/ausiasmarch/sohserver/api/TipoEventoController.java
+++ src/main/java/net/ausiasmarch/sohserver/api/TipoEventoController.java
@@ -1,5 +1,14 @@
 package net.ausiasmarch.sohserver.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import net.ausiasmarch.sohserver.service.TipoEventoService;
+
+@RestController
+@RequestMapping("/TipoEvento")
 public class TipoEventoController {
-    
+    @Autowired
+    TipoEventoService oTipoEventoService;
+
 }
