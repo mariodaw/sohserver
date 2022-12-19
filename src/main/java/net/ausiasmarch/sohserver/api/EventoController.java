8df1aee0f21f279b160ddff91412adb037988cf5
mario <newmariotomas@gmail.com>
2022-12-19 20:40:21 +0100

    server ahji ahi

--- src/main/java/net/ausiasmarch/sohserver/api/EventoController.java
+++ src/main/java/net/ausiasmarch/sohserver/api/EventoController.java
@@ -3,8 +3,11 @@ package net.ausiasmarch.sohserver.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +25,18 @@ public class EventoController {
         return new ResponseEntity<>(oEventoService.get(id), HttpStatus.OK);
     }
 
+    @GetMapping("/count")
+    public ResponseEntity<Long> count() {
+        return new ResponseEntity<Long>(oEventoService.count(), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Long> create(@RequestBody EventoEntity oNewEventoEntity) {
+        return new ResponseEntity<Long>(oEventoService.create(oNewEventoEntity), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> delete(@PathVariable(value = "id") Long id) {
+        return new ResponseEntity<Long>(oEventoService.delete(id), HttpStatus.OK);
+    }
 }
--- src/main/java/net/ausiasmarch/sohserver/service/EventoService.java
+++ src/main/java/net/ausiasmarch/sohserver/service/EventoService.java
@@ -4,10 +4,16 @@ package net.ausiasmarch.sohserver.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import net.ausiasmarch.sohserver.helper.ValidationHelper;
 import net.ausiasmarch.sohserver.entity.EventoEntity;
 import net.ausiasmarch.sohserver.exception.ResourceNotFoundException;
+import net.ausiasmarch.sohserver.exception.ResourceNotModifiedException;
 import net.ausiasmarch.sohserver.repository.EventoRepository;
 import net.ausiasmarch.sohserver.repository.TipoEventoRepository;
+
+import java.time.LocalDateTime;
+
+import javax.transaction.Transactional;
 
 @Service
 public class EventoService {
@@ -25,5 +31,65 @@ public class EventoService {
         return oEventoRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Evento with id: " + id + " not found"));
     }
+
+    public Long count() {
+       // oAuthService.OnlyAdmins();
+        return oEventoRepository.count();
+    }
+    public void validate(Long id) {
+        if (!oEventoRepository.existsById(id)) {
+            throw new ResourceNotFoundException("id " + id + " not exist");
+        }
+    }
+    public void validate(EventoEntity oEventoEntity) {
+        ValidationHelper.validateStringLength(oEventoEntity.getNombre(), 2, 50, "campo nombre de Evento (el campo debe tener longitud de 2 a 50 caracteres)");
+        ValidationHelper.validateDate(oEventoEntity.getFini(), LocalDateTime.of(1990, 01, 01, 00, 00, 00), LocalDateTime.of(2025, 01, 01, 00, 00, 00), "campo fecha de factura");
+        ValidationHelper.validateDate(oEventoEntity.getFfin(), LocalDateTime.of(1990, 01, 01, 00, 00, 00), LocalDateTime.of(2025, 01, 01, 00, 00, 00), "campo fecha de factura");
+        oTipoEventoService.validate(oEventoEntity.getTipoEvento().getId());
+        oTipoEventoService.validate(oEventoEntity.getId());
+    }
+    public Long create(EventoEntity oNewEventoEntity) {
+        //oAuthService.OnlyAdmins();
+        validate(oNewEventoEntity);
+        oNewEventoEntity.setId(0L);
+        return oEventoRepository.save(oNewEventoEntity).getId();
+    }
+
+    public Long update(EventoEntity oEventoEntity) {
+        validate(oEventoEntity.getId());
+        //oAuthService.OnlyAdminsOrOwnUsersData(oUsuarioEntity.getId());
+        validate(oEventoEntity);
+        oTipoEventoService.validate(oEventoEntity.getTipoEvento().getId());
+        //if (oAuthService.isAdmin()) {
+        //    return update4Admins(oUsuarioEntity).getId();
+        //} else {
+            return update4Users(oEventoEntity).getId();
+        //}
+    }
+    @Transactional
+    private EventoEntity update4Users(EventoEntity oUpdatedEventoEntity) {
+        EventoEntity oEventoEntity = oEventoRepository.findById(oUpdatedEventoEntity.getId()).get();
+        //keeping login password token & validado descuento activo tipousuario
+        oEventoEntity.setNombre(oUpdatedEventoEntity.getNombre());
+        oEventoEntity.setFini(oUpdatedEventoEntity.getFini());
+        oEventoEntity.setFfin(oUpdatedEventoEntity.getFfin());
+        oEventoEntity.setTipoEvento(oTipoEventoService.get(2L));
+        return oEventoRepository.save(oEventoEntity);
+    }
+
+    public Long delete(Long id) {
+        //oAuthService.OnlyAdmins();
+        if (oEventoRepository.existsById(id)) {
+            oEventoRepository.deleteById(id);
+            if (oEventoRepository.existsById(id)) {
+                throw new ResourceNotModifiedException("can't remove register " + id);
+            } else {
+                return id;
+            }
+        } else {
+            throw new ResourceNotModifiedException("id " + id + " not exist");
+        }
+    }
+  
     
 }
